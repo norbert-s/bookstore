b4940f0eea03dd10f3fc1ce2bbad554f468de07d
susztek.norbert <susztek.norbert@gmail.com>
2022-04-16 13:00:57 +0200

    refact without description

--- src/main/java/com/norbert/susztek/bookstore/dao/BookRepository.java
+++ src/main/java/com/norbert/susztek/bookstore/dao/BookRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface BookRepository extends CrudRepository<Book,Integer> {
     public List<Book> findAllByTitle(String title);
     public List<Book> findAllByAuthor(String author);
+    
 }
--- src/main/java/com/norbert/susztek/bookstore/entity/Book.java
+++ src/main/java/com/norbert/susztek/bookstore/entity/Book.java
@@ -19,43 +19,49 @@ public class Book implements Serializable {
     @Column(name="summary")
     private String summary;
 
-    public Book(int id, String title, String lastName, String description, String summary) {
+    public Book() {
+
+    }
+
+    public Book(int id, String title, String author, String summary) {
         this.id = id;
         this.title = title;
-        this.author = lastName;
+        this.author = author;
         this.summary = summary;
     }
 
-    public Book() {
-
+    public Book(String title, String author, String summary) {
+        this.title = title;
+        this.author = author;
+        this.summary = summary;
     }
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getTitle() {
         return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getAuthor() {
         return author;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
     public String getSummary() {
         return summary;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 
     public void setSummary(String summary) {

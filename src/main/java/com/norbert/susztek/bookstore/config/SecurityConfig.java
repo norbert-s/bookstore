bf8aed74f0522d9cb2499722cc8fafa78f66ba6d
susztek.norbert <susztek.norbert@gmail.com>
2022-04-17 16:59:28 +0200

    basic authentication works

--- src/main/java/com/norbert/susztek/bookstore/config/SecurityConfig.java
+++ src/main/java/com/norbert/susztek/bookstore/config/SecurityConfig.java
@@ -1,53 +1,14 @@
 package com.norbert.susztek.bookstore.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
+    protected void configure(HttpSecurity http) throws Exception {
+        http
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-		// add our users for in memory authentication
-		
-		//UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-				
-				  .withUser("john").password("test123").roles("CUSTOMER");
-
-				 
-
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-
-		http
-				.authorizeRequests()
-				.antMatchers("/books").hasRole("CUSTOMER")
-				.antMatchers("/").permitAll()
-			.and()
-			.formLogin()
-				.loginPage("/showLoginView")
-				.loginProcessingUrl("/authenticateTheUser")
-				.permitAll()
-			.and()
-			.logout()
-				.logoutSuccessUrl("/")  // after logout redirect to landing page (root)
-				.permitAll();
-	}
+		.formLogin(form -> form
+                .loginPage("/login")
+                .permitAll()
+        );
+    }
 }
-
-
-
-
-
-
--- src/main/java/com/norbert/susztek/bookstore/controller/HomeController.java
+++ src/main/java/com/norbert/susztek/bookstore/controller/HomeController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class HomeController {
-    @GetMapping("/")
-    public String getHome(){
-        return "books/home";
-    }
+//    @GetMapping("/")
+//    public String getHome(){
+//        return "books/home";
+//    }
 
 }
